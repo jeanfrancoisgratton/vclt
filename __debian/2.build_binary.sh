9c094176232e9978dc687ad457ca820f17631095
jeanfrancoisgratton <jean-francois@famillegratton.net>
2026-06-18 19:35:29 -0400

    fixed binary name in DEBIAN build scripts

--- __debian/2.build_binary.sh
+++ __debian/2.build_binary.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PKGDIR="vclt-2.00.00-0_amd64"
+PKGDIR="vclt-2.00.00-1_amd64"
 
 mkdir -p ${PKGDIR}/opt/bin ${PKGDIR}/DEBIAN
 mkdir -p ${PKGDIR}/opt/bin ${PKGDIR}/DEBIAN
@@ -10,9 +10,9 @@ done
 
 echo "Building binary from source"
 cd ../src
-go build -o ../__debian/${PKGDIR}/opt/bin/vclt2 .
-strip ../__debian/${PKGDIR}/opt/bin/vclt2
-sudo chown 0:0 ../__debian/${PKGDIR}/opt/bin/vclt2
+go build -o ../__debian/${PKGDIR}/opt/bin/vclt .
+strip ../__debian/${PKGDIR}/opt/bin/vclt
+sudo chown 0:0 ../__debian/${PKGDIR}/opt/bin/vclt
 
 echo "Binary built. Now packaging..."
 cd ../__debian/
