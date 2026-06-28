40200435299d10d31ee26dea1a2db552f46fc211
jeanfrancoisgratton <jean-francois@famillegratton.net>
2026-06-27 20:18:45 -0400

    fixed debian build script

--- __debian/2.build_binary.sh
+++ __debian/2.build_binary.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PKGDIR="vclt-2.3.0-0_amd64"
+BINARYNAME=vclt
 
 mkdir -p ${PKGDIR}/opt/bin ${PKGDIR}/DEBIAN
 mkdir -p ${PKGDIR}/opt/bin ${PKGDIR}/DEBIAN
@@ -10,9 +11,9 @@ done
 
 echo "Building binary from source"
 cd ../src
-go build -o ../__debian/${PKGDIR}/opt/bin/vclt .
-strip ../__debian/${PKGDIR}/opt/bin/vclt
-sudo chown 0:0 ../__debian/${PKGDIR}/opt/bin/vclt
+go build -trimpath -mod=readonly -modcacherw -ldflags="-s -w -buildid=" -o "${BINARYNAME}" .
+strip ../__debian/${PKGDIR}/opt/bin/"${BINARYNAME}"
+sudo chown 0:0 ../__debian/${PKGDIR}/opt/bin/"${BINARYNAME}"
 
 echo "Binary built. Now packaging..."
 cd ../__debian/
